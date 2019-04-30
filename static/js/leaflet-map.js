6e94565f50992be9cd8f03f7d219cad404e00720
webuimaker <webuimaker@gmail.com>
2019-04-30 17:17:50 +0530

    updated

--- static/js/leaflet-map.js
+++ static/js/leaflet-map.js
@@ -1,11 +1,11 @@
 function initMap() {
-    var latLong = [50.084750, 8.247026]; // Wiesbaden
+    var latLong = [-27.390660, 153.056580]; // Wiesbaden
 
-    var map = L.map("map").setView(latLong, 5);
+    var map = L.map("map").setView(latLong, 8);
 
     L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-      maxZoom: 18,
+      maxZoom: 15,
     }).addTo(map);
 
     var icon = L.icon({
